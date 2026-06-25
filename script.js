0ee74d6c8f3be342335de03c1759056bf89b80c4
DakshimArora <dakshim.official@gmail.com>
2026-06-25 13:50:12 +0530

    Added local storage and persistence for theme and counter

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
 const btn=document.getElementById("btn")
 const counter=document.getElementById("counter")
-let count=0
+let count=Number(localStorage.getItem("count")) || 0
+counter.textContent=`Theme changed ${count} times`
 let isDark=false;
+const savedTheme=localStorage.getItem("theme")
+if(savedTheme==="dark"){
+    document.body.style.backgroundColor="black"
+    document.body.style.color="white"
+    btn.textContent="Light mode"
+    
+    isDark=true 
+}
 btn.addEventListener("click",function(){
     if(isDark){
         document.body.style.backgroundColor="white"
         document.body.style.color="black"
         isDark=false
+        localStorage.setItem("theme","light")
         btn.textContent="Dark mode"
     }
     else{
         document.body.style.backgroundColor="black"
         document.body.style.color="white"
         isDark=true
+        localStorage.setItem("theme","dark")
         btn.textContent="Light mode"
     }
     count=count+1
+    localStorage.setItem("count",count)
     counter.textContent=`Theme changed ${count} times`
 })
